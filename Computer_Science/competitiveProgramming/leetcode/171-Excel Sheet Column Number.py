d36c4f05c37bed8e483d0ec2ea471fc40c287f7d
Calvin Mozola <calvin@stuntfactory.com>
2022-02-23 23:22:29 -0500

    Added fast solution to leetcode-171

--- Computer_Science/competitiveProgramming/leetcode/171-Excel Sheet Column Number.py
+++ Computer_Science/competitiveProgramming/leetcode/171-Excel Sheet Column Number.py
@@ -1,6 +1,10 @@
 # Runtime: 55 ms, faster than 32.47% of Python3 online submissions for Excel Sheet Column Number.
 # Memory Usage: 13.8 MB, less than 70.02% of Python3 online submissions for Excel Sheet Column Number.
 
+# Second soltuion
+# Runtime: 32 ms, faster than 92.31% of Python3 online submissions for Excel Sheet Column Number.
+# Memory Usage: 13.9 MB, less than 70.02% of Python3 online submissions for Excel Sheet Column Number.
+
 import string
 
 class Solution:
@@ -9,6 +13,14 @@ class Solution:
         for letter in columnTitle:
             col_num *= len(string.ascii_uppercase)
             current_val = string.ascii_uppercase.index(letter) + 1
+            col_num += current_val
+        return col_num
+    def titleToNumber_second_solution(self, columnTitle: str) -> int:
+        col_num = 0
+        alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        for letter in columnTitle:
+            col_num *= len(alpha)
+            current_val = ord(letter) - 64
             col_num += current_val
         return col_num
 print(string.ascii_uppercase)
